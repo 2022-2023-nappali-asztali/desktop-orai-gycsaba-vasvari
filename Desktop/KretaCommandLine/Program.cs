ac26f206551ea696e6cd8b8d2202c89bfbd0d8ed
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-08 12:08:43 +0200

    Parancssori projekt létrejött

---
+++ Desktop/KretaCommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KretaCommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
