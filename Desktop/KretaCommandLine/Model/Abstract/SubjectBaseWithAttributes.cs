4898a313789a8ca60e86ed85d80c38ebf86ddf1a
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-09 10:21:01 +0200

    Display name

--- Desktop/KretaCommandLine/Model/Abstract/SubjectBaseWithAttributes.cs
+++ Desktop/KretaCommandLine/Model/Abstract/SubjectBaseWithAttributes.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace KretaCommandLine.Model.Abstract
 {
     class SubjectBaseWithAttributes : SubjectBase
     {
         [Column("name")]
+        [Display(Name = "Subject name:")]
         public override string SubjectName 
         { 
             get => base.SubjectName; 
