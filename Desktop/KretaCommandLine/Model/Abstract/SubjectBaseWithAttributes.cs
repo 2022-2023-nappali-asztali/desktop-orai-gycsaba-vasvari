cf5b53c3db3c016a56b5a0c8d9a2d03b8f7b96db
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-09 10:19:17 +0200

    SubjectName adatbázis osztlop és ovveride

--- Desktop/KretaCommandLine/Model/Abstract/SubjectBaseWithAttributes.cs
+++ Desktop/KretaCommandLine/Model/Abstract/SubjectBaseWithAttributes.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KretaCommandLine.Model.Abstract
 {
     class SubjectBaseWithAttributes : SubjectBase
     {
+        [Column("name")]
+        public override string SubjectName 
+        { 
+            get => base.SubjectName; 
+            set => base.SubjectName = value; 
+        }
     }
 }
