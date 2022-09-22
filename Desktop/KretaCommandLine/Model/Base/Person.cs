76355cd30b32024486c423122110c009c1250806
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-22 12:07:12 +0200

    Abstract Person osztály

---
+++ Desktop/KretaCommandLine/Model/Base/Person.cs
@@ -0,0 +1,28 @@
+﻿using KretaCommandLine.Model.Interface.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KretaCommandLine.Model.Base
+{
+    public abstract class Person : IId, IEquatable<Person>, IComparable
+    {
+        public long Id { get; set;}
+   
+        private IPerson person;
+        private IAddress address;
+        private IAccount account;
+
+        public bool Equals(Person other)
+        {
+            return false;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return 0;
+        }
+    }
+}
