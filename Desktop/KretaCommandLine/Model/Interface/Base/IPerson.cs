a1c5cf6b98792238e17fa462a16e1b3a676de685
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-22 11:16:35 +0200

    IPerson interface kész

--- Desktop/KretaCommandLine/Model/Interface/Base/IPerson.cs
+++ Desktop/KretaCommandLine/Model/Interface/Base/IPerson.cs
@@ -14,5 +14,38 @@ namespace KretaCommandLine.Model.Interface.Base
         public bool Wooman { get; set; }
 
         public DateTime DataOfBirth { get; set; }
+
+        public string HungarianFullName
+        {
+            get => $"{this.LastName} {this.FirstName}";
+        }
+
+        public string FullName
+        {
+            get => $"{this.FirstName} {this.LastName}";
+        }
+
+        public bool IsWoman
+        {
+            get => this.Wooman;
+        }
+
+        public bool IsMan
+        {
+            get => !this.Wooman;
+        }
+
+        public int Age
+        {
+            get
+            {
+                int age = 0;
+                age = DateTime.Now.Year - DataOfBirth.Year;
+                if (DateTime.Now.DayOfYear < DataOfBirth.DayOfYear)
+                    age = age - 1;
+                return age;
+            }
+        }
+
     }
 }
