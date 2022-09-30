7803f0bb59470a4f6eaeee84654239bd8ff97f16
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-30 13:12:41 +0200

    Működnek a metódusok

--- Desktop/KretaCommandLine/Model/Subject.cs
+++ Desktop/KretaCommandLine/Model/Subject.cs
@@ -8,7 +8,16 @@ using KretaCommandLine.Model.Abstract;
 
 namespace KretaCommandLine.Model
 {
-    public class Subject : SubjectBase
+    public class Subject : SubjectBase, IEquatable<Subject>, IComparable
     {
+        public int CompareTo(object obj)
+        {
+            return 0;
+        }
+
+        public bool Equals(Subject other)
+        {
+            return false;
+        }
     }
 }
