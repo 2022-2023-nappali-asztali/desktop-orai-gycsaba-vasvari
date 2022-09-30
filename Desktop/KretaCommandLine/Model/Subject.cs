9c50c9d1468533e3e67f1245475d45d0d04ce57f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-30 13:10:50 +0200

    Subject osztály

---
+++ Desktop/KretaCommandLine/Model/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using KretaCommandLine.Model.Abstract;
+
+namespace KretaCommandLine.Model
+{
+    public class Subject : SubjectBase
+    {
+    }
+}
