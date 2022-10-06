ba1bce0310ac6157f0de1052e6f29873e6110dab
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-10-06 09:27:56 +0200

    XUnit projekt és SubjectTest

---
+++ Desktop/KretaDesktop/KretaCommandLineTestXUnit/Model/SubjecTest.cs
@@ -0,0 +1,15 @@
+using KretaCommandLine.Model;
+using System;
+using Xunit;
+
+namespace KretaCommandLineTestXUnit
+{
+    public class SubjecTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Subject
+        }
+    }
+}
