660045093df9644b736f063b97ae75281b55ddd6
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-11-23 11:31:46 +0100

    UpdateViewCommand

--- Desktop/KretaDesktop/KretaDesktop/ViewModel/MainWindowViewModel.cs
+++ Desktop/KretaDesktop/KretaDesktop/ViewModel/MainWindowViewModel.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using KretaDesktop.ViewModel.BaseClass;
+
 namespace KretaDesktop.ViewModel
 {
     class MainWindowViewModel
     {
+        public RelayCommand UpdateViewCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            UpdateViewCommand = new RelayCommand((parameter) => UpdateView(parameter));
+        }
+
+        void UpdateView(object parameter)
+        {
+            if (parameter is string)
+            {
+                string commandParameter=(string)parameter;
+                if (commandParameter == "Exit")
+                {
+                    ;
+                }
+            }
+        }
     }
 }
