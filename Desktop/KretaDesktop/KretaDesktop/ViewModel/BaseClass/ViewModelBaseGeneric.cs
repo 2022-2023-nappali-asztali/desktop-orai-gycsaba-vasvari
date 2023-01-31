253f724291ed0df0a406f9627b880ce515b7311f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-01-31 10:38:24 +0100

    ICollectionBase

---
+++ Desktop/KretaDesktop/KretaDesktop/ViewModel/BaseClass/ViewModelBaseGeneric.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using KretaCommandLine.Model;
+
+namespace KretaDesktop.ViewModel.BaseClass
+{
+    public class ViewModelBase<TEntity, TCollection> 
+        where TEntity : class
+        where TCollection : ICollection<TEntity>
+    {
+
+        private TCollection collection;
+
+        public void Add(TEntity entiy)
+        {
+            collection.Add(entiy);
+        }
+    }
+
+    public class SubjectViewModel : ViewModelBase<Subject,ObservableCollection<Subject>>
+    {
+        Subject subject = new Subject(1, "Töri");
+
+        public SubjectViewModel()
+        {
+            Add(subject);
+        }
+    }
+
+    public class StudentViewModel : ViewModelBase<Student, ObservableCollection<Student>>
+    {
+        Student student = new Student();
+
+        public StudentViewModel()
+        {
+            Add(student);
+        }
+
+    }
+        
+
+
+
+}
