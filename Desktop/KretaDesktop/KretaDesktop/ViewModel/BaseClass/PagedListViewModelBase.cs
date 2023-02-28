320f4451b3a78079b0c3be3ac2f1b238312bb484
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-02-28 10:50:50 +0100

    Terv

---
+++ Desktop/KretaDesktop/KretaDesktop/ViewModel/BaseClass/PagedListViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using KretaCommandLine.APIModel;
+using KretaCommandLine.Model.Abstract;
+
+namespace KretaDesktop.ViewModel.BaseClass
+{
+    public class PagedListViewModelBase<TEntity,TPagedList> : ViewModelBase<TEntity,TPagedList>
+        where TEntity : ClassWithId,new()
+        where TPagedList : PagedList<TEntity>, new()
+    {
+    }
+}
