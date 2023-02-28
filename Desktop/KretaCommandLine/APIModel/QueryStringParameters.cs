db879f4af7c85adff4077c69440d258c311da81a
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-02-28 10:09:03 +0100

    QueryStringParameters

---
+++ Desktop/KretaCommandLine/APIModel/QueryStringParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KretaCommandLine.APIModel
+{
+    public class QueryStringParameters
+    {
+        public int NumberOfPage { get; set; }
+        public int NumberOfItems { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+
+        public bool HasPreviousPage => CurrentPage > 0;
+        public bool HasNextPage => CurrentPage < NumberOfPage;
+    }
+}
