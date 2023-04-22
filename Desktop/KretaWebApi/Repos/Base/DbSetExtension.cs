dc0ce57e456df773a99f6a3754f7dc049dc78c8d
gycsaba <gyuris.csaba@vasvari.org>
2023-04-22 09:56:09 +0200

    FiltringAndSorting megoldva ienumerable-val

--- Desktop/KretaWebApi/Repos/Base/DbSetExtension.cs
+++ Desktop/KretaWebApi/Repos/Base/DbSetExtension.cs
@@ -1,7 +1,9 @@
 ﻿using KretaCommandLine.QueryParameter;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
 using System.Reflection;
+using System.Text;
+
+using System.Linq.Dynamic.Core;
 
 namespace KretaWebApi.Repos.Base
 {
@@ -42,8 +44,8 @@ namespace KretaWebApi.Repos.Base
                     try
                     {
                         var list = await dbSet.ToListAsync();
-                        var lowerCaseSearchTerm = queryParameters.SearchTerm.Trim().ToLower();
-                        result = list.Where(entity => propertyInfo.GetValue(entity, null).ToString().ToLower().Contains(lowerCaseSearchTerm)).ToList();
+                        string lowerCaseSearchTerm = queryParameters.SearchTerm.Trim().ToLower();
+                        result = list.Where(entity => propertyInfo.GetValue(entity, null).ToString().ToLower().Contains(lowerCaseSearchTerm)).ApplySort(queryParameters.OrderBy).ToList();
                     }
                     catch (Exception e)
                     {
@@ -51,6 +53,59 @@ namespace KretaWebApi.Repos.Base
                 }
             }
             return result;
+        }
+
+        public static IQueryable<TEntity> ApplySort<TEntity>(this IQueryable<TEntity> entities, string orderByQueryString) where TEntity : class
+        {
+            if (!entities.Any())
+                return entities;
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+            {
+                return entities;
+            }
+            var orderParams = orderByQueryString.Trim().Split(',');
+            var propertyInfos = typeof(TEntity).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+                if (objectProperty == null)
+                    continue;
+                var sortingOrder = param.EndsWith(" desc") ? "descending" : "ascending";
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {sortingOrder}, ");
+            }
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            return entities.OrderBy(orderQuery);
+        }
+
+        public static IEnumerable<TEntity> ApplySort<TEntity>(this IEnumerable<TEntity> entities, string orderByQueryString) where TEntity : class
+        {
+            if (!entities.Any())
+                return entities;
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+            {
+                return entities;
+            }
+            var orderParams = orderByQueryString.Trim().Split(',');
+            var propertyInfos = typeof(TEntity).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+                if (objectProperty == null)
+                    continue;
+                var sortingOrder = param.EndsWith(" desc") ? "descending" : "ascending";
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {sortingOrder}, ");
+            }
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            // https://stackoverflow.com/questions/41244/dynamic-linq-orderby-on-ienumerablet-iqueryablet
+            return entities.AsQueryable().OrderBy(orderQuery);
         }
     }
 }
