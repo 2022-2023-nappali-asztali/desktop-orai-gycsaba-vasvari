c766464491a262045ea554421c0536d7b4e7cbfb
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-04-28 10:20:09 +0200

    Átalakítások után minden funkció működik

--- Desktop/KretaWebApi/Controllers/ScoolClassController.cs
+++ Desktop/KretaWebApi/Controllers/ScoolClassController.cs
@@ -7,10 +7,32 @@ namespace KretaWebApi.Controllers
 {
     [ApiController]
     [Route("/api/[controller]")]
-    public class SchoolClassController : ClassWithIdBaseController<SchoolClass>
+    public class SchoolClassController : IncludedBaseController<SchoolClass>
     {
-        public SchoolClassController(IIncludedRepoBase service, IRepoBase repoBaseService) : base(service, repoBaseService)
+        private ISchoolClassRepoBase _service;
+
+        public SchoolClassController(ISchoolClassRepoBase service, IIncludedRepoBase includedService, IClassWithIdRepoBase classWithIdRepoBase, IRepoBase repoBaseService)
+            : base(includedService, classWithIdRepoBase, repoBaseService)
         {
+            _service = service;
+        }
+
+
+
+        [HttpGet("included")]
+        public async Task<ActionResult<List<SchoolClass>>> SelectAllIncludedRecordAsync()
+        {
+            List<SchoolClass>? entitys = null;
+            try
+            {
+                entitys = await _service.SelectAllIncludedRecordAsync<SchoolClass>(null);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Az adatbázis nem elérhető.");
+            }
+            return Ok(entitys);
         }
     }
 }
