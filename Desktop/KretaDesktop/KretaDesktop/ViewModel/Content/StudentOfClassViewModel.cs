756ef84df3ff97a2a3f00a8a6015c829a6c69222
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-05-03 09:41:31 +0200

    Osztáyok adatai megjelennek

--- Desktop/KretaDesktop/KretaDesktop/ViewModel/Content/StudentOfClassViewModel.cs
+++ Desktop/KretaDesktop/KretaDesktop/ViewModel/Content/StudentOfClassViewModel.cs
@@ -1,13 +1,32 @@
-﻿using KretaDesktop.ViewModel.BaseClass;
+﻿using KretaCommandLine.Model;
+using KretaDesktop.Services;
+using KretaDesktop.ViewModel.BaseClass;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace KretaDesktop.ViewModel.Content
 {
-    public class StudentOfClassViewModel : InitializedViewModelBase
+    public class StudentOfClassViewModel : ServiceViewModelBase<SchoolClass>
     {
-        public override Task OnInitialize()
+        private IAPIService _service;
+        private IWrapService _wrapService;
+
+        private List<SchoolClass> _schoolClasses;
+        public List<SchoolClass> SchoolClasses
         {
-            return Task.CompletedTask;
+            get { return _schoolClasses; }
+            set { SetValue(ref _schoolClasses, value); }
+        }
+
+        public StudentOfClassViewModel(IAPIService service, IWrapService wrapService) : base(service)
+        {
+            _service = service;
+            _wrapService = wrapService;
+        }
+
+        public async override Task OnInitialize()
+        {
+            SchoolClasses= await _service.SelectAllRecordAsync<SchoolClass>();
         }
     }
 }
